167c392fe2be6b828528878b4e6bd8418d4bb652
lc <ldchao@qq.com>
2025-11-17 23:03:30 +0800

    ./gradlew assembleMod_box4319_adminRelease assembleMod_box4319_huaweiRelease assembleMod_box4319_oppoRelease assembleMod_box4319_vivoRelease

--- src/main/java/com/box/common/network/SendHeadInterceptor.kt
+++ src/main/java/com/box/common/network/SendHeadInterceptor.kt
@@ -14,13 +14,13 @@ class SendHeadInterceptor : Interceptor {
             if (url.startsWith("/api")) {
                 appViewModel.modUserInfo.value?.let {
                     builder.addHeader("token", it.token).build()
-                    //builder.addHeader("Authorization", it.token).build()
+                    builder.addHeader("Authorization", it.token).build()
                 }
             }
         }
         if (url.contains("userCenter")) {
             appViewModel.modUserInfo.value?.let {
-                builder.addHeader("Authorization", it.token).build()
+                //builder.addHeader("Authorization", it.token).build()
             }
 
         }
